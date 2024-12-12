fbf7911faf709f19a2601c977f99c0a6bfc0d924
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-12-12 10:49:07 +0100

    adding MapView

--- SwiftUIProject7-Africa/App/MapView.swift
+++ SwiftUIProject7-Africa/App/MapView.swift
@@ -5,11 +5,31 @@
 //  Created by Yaro Paul  on 09/12/2024.
 //
 
+import MapKit
 import SwiftUI
 
 struct MapView: View {
+    @State private var cameraPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: 6.600286, longitude: 16.43775599),
+            span: MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+        )
+    )
+
+    let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+
     var body: some View {
-        Text("Map")
+        Map(position: $cameraPosition) {
+            ForEach(locations, id: \.id) { item in
+                Annotation("Location \(item.id)", coordinate: item.location) {
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 32, height: 32, alignment: .center)
+                }
+            }
+        }
     }
 }
 
