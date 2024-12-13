c1da93cfddf890ccda0bea52ade9ba390dbe370c
Paul Yaro <alassane-paul.yaro@outlook.fr>
2024-12-13 11:12:41 +0100

    upgrate GalleryView

--- SwiftUIProject7-Africa/App/GalleryView.swift
+++ SwiftUIProject7-Africa/App/GalleryView.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct GalleryView: View {
+
+    @State private var isAnimating: Bool = false
+    @State private var selectedAnimal: String = "lion"
+
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+
+    func gridSwitch() {
+        gridLayout = Array(
+            repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack(alignment: .center, spacing: 30) {
+
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, initial: true) {
+                        oldValue, newValue in
+                        gridSwitch()
+                    }
+
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10)
+                {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    }
+                }  
+                .animation(.easeIn, value: isAnimating)
+                .onAppear(perform: {
+                    isAnimating = true
+                    gridSwitch()
+                })
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
